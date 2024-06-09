336122927852fa52f90a675f7f1b22af81db0414
nikhilkumarmishra <nikhilkumarmishra766@gmail.com>
2024-06-09 17:17:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/navapp/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.example.navapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_home.dsaCardView
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Set OnClickListener on the DSA card view
+        dsaCardView.setOnClickListener {
+            val intent = Intent(activity, DsaActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
